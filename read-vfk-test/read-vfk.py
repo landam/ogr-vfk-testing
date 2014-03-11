b71227c626ecb18f4544de2c9702cc28bca7e91a
Martin Landa <landa.martin@gmail.com>
2014-03-11 11:27:05 +0100

    implement second pass (read from DB)

--- read-vfk-test/read-vfk.py
+++ read-vfk-test/read-vfk.py
@@ -29,8 +29,6 @@ def init_ogr():
     os.environ['OGR_VFK_DB_NAME'] = OUTPUT_DB
     # do not delete output data when closing datasource
     os.environ['OGR_VFK_DB_DELETE'] = 'NO'
-    # overwrite existing SQLite database when reading VFK data
-    os.environ['OGR_VFK_DB_OVERWRITE'] = 'YES'
     # enable debug messages (?)
     os.environ['CPL_DEBUG'] = 'OFF'
     # enable read per data block (will be slower)
@@ -86,42 +84,58 @@ def check_vfk_line(idx, line):
     except (IndexError, KeyError):
         pass
 
-def main():
-    # initialize OGR library for this script
-    init_ogr()
-    
-    t0 = time.clock()
-
-    # open VFK file as an OGR datasource
-    print "Reading '%s'..." % INPUT_VFK
+def read_vfk(suppress_output=False):
     ds = open_vfk(INPUT_VFK)
-    print_delimiter()
-
-    # get list of OGR layers (ie. VFK blocks &B)
+    
     nlayers = ds.GetLayerCount()
     for lidx in range(nlayers):
         layer =  ds.GetLayer(lidx)
         gtype = ogr.GeometryTypeToName(layer.GetGeomType()).lower()
         if gtype == 'none':
             gtype = ' ' * 8
-
+        
         if not layer:
             fatal_error("Unable to get %d layer" % lidx)
-        print "Fetching %-6s ... %6d %11s features detected" % \
-            (layer.GetName(), layer.GetFeatureCount(),
-             gtype)
+        if not suppress_output:
+            print "Fetching %-6s ... %6d %11s features detected" % \
+                (layer.GetName(), layer.GetFeatureCount(),
+                 gtype)
     
-    print_delimiter()
-    print time.clock() - t0, "seconds process time"
-    print_delimiter()
-    print "Number of processed records:"
-    for key, value in NUM_OF_LINES.iteritems():
-        print "\t%s: %d" % (key, value)
-    print_delimiter()
-
     # close OGR datasource (flush memory)
     ds.Destroy()
     
+def main():
+    # initialize OGR library for this script
+    init_ogr()
+    
+    # open VFK file as an OGR datasource
+    print "Reading '%s'..." % INPUT_VFK
+
+    # first pass
+    print_delimiter()
+    t0 = time.clock()
+    # overwrite existing SQLite database when reading VFK data
+    os.environ['OGR_VFK_DB_OVERWRITE'] = 'YES'
+    read_vfk()          # get list of OGR layers (ie. VFK blocks &B)
+
+    print_delimiter()
+    print time.clock() - t0, "seconds process time (first pass - from VFK file)"
+    print_delimiter()
+
+    # second pass
+    t0 = time.clock()
+    # read data from DB
+    os.environ['OGR_VFK_DB_OVERWRITE'] = 'NO'
+    read_vfk(suppress_output=True)
+
+    print time.clock() - t0, "seconds process time (second pass - from SQLite DB)"
+    print_delimiter()
+
+    # print "Number of processed records:"
+    # for key, value in NUM_OF_LINES.iteritems():
+    #     print "\t%s: %d" % (key, value)
+    # print_delimiter()
+
     return 0
 
 if __name__ == "__main__":
