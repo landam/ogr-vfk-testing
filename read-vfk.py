a2eb3649d50b7663a924adf8aa524155573bd757
Martin Landa <landa.martin@gmail.com>
2014-09-09 19:04:44 +0200

    add cli interface, avoid global variables

--- read-vfk.py
+++ read-vfk.py
@@ -4,8 +4,6 @@ This script reads VFK data using OGR library. VFK data are converted
 into SQLite database.
 
 Reference: http://gdal.org/ogr/drv_vfk.html
-
-No CLI interface. Change global variables below.
 """
 
 import os
@@ -14,10 +12,6 @@ import time
 
 from osgeo import ogr, gdal
 
-# global variables - input VFK file
-INPUT_VFK = os.path.join(os.environ['HOME'], 'geodata', 'vfk', 'exportvse.vfk')
-# global variables - output SQLite database
-OUTPUT_DB = os.path.join(os.environ['HOME'], 'geodata', 'vfk', 'exportvse.db')
 # global variables - check VFK line
 NUM_OF_LINES = { 'B' : 0,
                  'D' : 0,
@@ -25,10 +19,6 @@ NUM_OF_LINES = { 'B' : 0,
 
 def init_ogr():
     """Initialize OGR library - set environmental variables"""
-    # set DB name (default is <vfk_file>.db>)
-    os.environ['OGR_VFK_DB_NAME'] = OUTPUT_DB
-    # do not delete output data when closing datasource
-    os.environ['OGR_VFK_DB_DELETE'] = 'NO'
     # enable debug messages (?)
     os.environ['CPL_DEBUG'] = 'OFF'
     # enable read per data block (will be slower)
@@ -84,8 +74,8 @@ def check_vfk_line(idx, line):
     except (IndexError, KeyError):
         pass
 
-def read_vfk(suppress_output=False):
-    ds = open_vfk(INPUT_VFK)
+def read_vfk(filename, suppress_output=False):
+    ds = open_vfk(filename)
     
     nlayers = ds.GetLayerCount()
     for lidx in range(nlayers):
@@ -105,19 +95,19 @@ def read_vfk(suppress_output=False):
     # close OGR datasource (flush memory)
     ds.Destroy()
     
-def main():
+def main(filename):
     # initialize OGR library for this script
     init_ogr()
     
     # open VFK file as an OGR datasource
-    print "Reading '%s'..." % INPUT_VFK
+    print "Reading '%s'..." % filename
 
     # first pass
     print_delimiter()
     t0 = time.clock()
     # overwrite existing SQLite database when reading VFK data
     os.environ['OGR_VFK_DB_OVERWRITE'] = 'YES'
-    read_vfk()          # get list of OGR layers (ie. VFK blocks &B)
+    read_vfk(filename)          # get list of OGR layers (ie. VFK blocks &B)
 
     print_delimiter()
     print time.clock() - t0, "seconds process time (first pass - from VFK file)"
@@ -127,7 +117,7 @@ def main():
     t0 = time.clock()
     # read data from DB
     os.environ['OGR_VFK_DB_OVERWRITE'] = 'NO'
-    read_vfk(suppress_output=True)
+    read_vfk(filename, suppress_output=True)
 
     print time.clock() - t0, "seconds process time (second pass - from SQLite DB)"
     print_delimiter()
@@ -140,4 +130,12 @@ def main():
     return 0
 
 if __name__ == "__main__":
-    sys.exit(main())
+    if len(sys.argv) != 2:
+        print "Usage: read_vfk.py path/to/vfk/file"
+        sys.exit(1)
+    
+    filename = sys.argv[1]
+    if not os.path.exists(filename):
+        fatal_error("File '%s' not found" % filename)
+    
+    sys.exit(main(filename))
